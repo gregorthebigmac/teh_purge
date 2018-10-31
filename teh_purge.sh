f65639d316f44de85f5ee5225005af85fffc4ec9
gregorthebigmac <gregorthebigmac@gmail.com>
2018-10-31 15:03:13 -0500

    added script

---
+++ teh_purge.sh
@@ -0,0 +1,29 @@
+# list of software to uninstall
+sudo apt purge -y aisleriot
+sudo apt purge -y brasero
+sudo apt purge -y caribou
+sudo apt purge -y cheese
+sudo apt purge -y deja-dup
+sudo apt purge -y empathy
+sudo apt purge -y evolution
+sudo apt purge -y gnome-accessibility-profiles
+sudo apt purge -y gnome-accessibility-themes
+sudo apt purge -y gnome-calendar
+sudo apt purge -y gnome-contacts
+sudo apt purge -y gnome-documents
+sudo apt purge -y gnome-getting-started-docs
+sudo apt purge -y gnome-mahjongg
+sudo apt purge -y gnome-maps
+sudo apt purge -y gnome-mines
+sudo apt purge -y gnome-music
+sudo apt purge -y gnome-orca
+sudo apt purge -y gnome-online-accounts
+sudo apt purge -y gnome-online-miners
+sudo apt purge -y gnome-photos
+sudo apt purge -y gnome-sudoku
+sudo apt purge -y gnome-user-guide
+sudo apt purge -y gnome-weather
+sudo apt purge -y libreoffice*
+sudo apt purge -y rhythmbox
+sudo apt purge -y totem
+
