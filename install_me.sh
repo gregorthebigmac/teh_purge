8fe7fd7a01b8ce6d91cfdc4a636b962b129dfe04
Gregor <gregorthebigmac@gmail.com>
2018-11-05 23:50:10 -0600

    removed bad flag from line 7.

--- install_me.sh
+++ install_me.sh
@@ -4,7 +4,7 @@ sudo apt-get install arp-scan -y
 sudo apt-get install htop -y
 sudo apt-get install openssh-server -y
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/ -y
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt-get install apt-transport-https -y
 sudo apt-get update && sudo apt upgrade -y
