6cc176060696dd9f837f6e820b884b75d419f8cf
Gregor <gregorthebigmac@gmail.com>
2018-11-06 00:03:17 -0600

    added packages removed automatically when caribou is removed.

--- install_me.sh
+++ install_me.sh
@@ -1,11 +1,17 @@
-sudo apt-get install git -y
-sudo apt-get install curl -y
-sudo apt-get install arp-scan -y
-sudo apt-get install htop -y
-sudo apt-get install openssh-server -y
+sudo apt-get install -y gmd3
+sudo apt-get install -y chrome-gnome-shell
+sudo apt-get install -y gnome-shell-extensions
+sudo apt-get install -y update-notifier
+sudo apt-get install -y gnome-shell
+sudo apt-get install -y update-manager
+sudo apt-get install -y ubuntu-release-upgrader-gtk
+sudo apt-get install -y curl
+sudo apt-get install -y arp-scan
+sudo apt-get install -y htop
+sudo apt-get install -y openssh-server
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get install apt-transport-https -y
-sudo apt-get update && sudo apt upgrade -y
-sudo apt-get install code -y
+sudo apt-get install -y apt-transport-https
+sudo apt-get update && sudo apt upgrade
+sudo apt-get install -y code
