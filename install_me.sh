dccf220140124f253f127e876b9f28cc54ef363b
gregorthebigmac <gregorthebigmac@gmail.com>
2018-12-21 14:08:12 -0600

    removed sudo from beginning of each line. User is intended to run entire script as sudo.

--- install_me.sh
+++ install_me.sh
@@ -1,10 +1,10 @@
-sudo apt-get install -y curl
-sudo apt-get install -y arp-scan
-sudo apt-get install -y htop
-sudo apt-get install -y openssh-server
+apt-get install -y curl
+apt-get install -y arp-scan
+apt-get install -y htop
+apt-get install -y openssh-server
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get install -y apt-transport-https
-sudo apt-get update && sudo apt upgrade
-sudo apt-get install -y code
+install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+apt-get install -y apt-transport-https
+apt-get update && sudo apt upgrade
+apt-get install -y code
